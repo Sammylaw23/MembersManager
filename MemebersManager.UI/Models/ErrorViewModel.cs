b855985f60269795f3832fc77dfba412ac5a03ea
Sammylaw23 <lawalsamuel2310@gmail.com>
2021-07-08 03:02:12 +0100

    Initial Commit

---
+++ MemebersManager.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MemebersManager.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
